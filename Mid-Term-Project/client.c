edfd331482c5e91e21d46aae1a139f0944b5bc2d
sreekar <33954702+sreekarappana1998@users.noreply.github.com>
2020-03-02 21:42:28 +0530

    Create client.c

---
+++ Mid-Term-Project/client.c
@@ -0,0 +1,127 @@
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <sys/socket.h> 
+#define MAX 80 
+#define PORT 8080 
+#define SA struct sockaddr 
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <unistd.h>
+#include <errno.h>
+
+#include <pulse/simple.h>
+#include <pulse/error.h>
+#include <pulse/gccmacro.h>
+
+#define BUFSIZE 1024
+
+/* A simple routine calling UNIX write() in a loop */
+static ssize_t loop_write(int fd, const void*data, size_t size) {
+    ssize_t ret = 0;
+
+    while (size > 0) {
+        ssize_t r;
+
+        if ((r = write(fd, data, size)) < 0)
+            return r;
+
+        if (r == 0)
+            break;
+
+        ret += r;
+        data = (const uint8_t*) data + r;
+        size -= (size_t) r;
+    }
+
+    return ret;
+}
+
+
+void func(int s) 
+{ 
+	//char buff[MAX]; 
+	int n; 
+	
+	/* The sample type to use */
+    static const pa_sample_spec ss = {
+        .format = PA_SAMPLE_S16LE,
+        .rate = 44100,
+        .channels = 2
+    };
+    
+    pa_simple *s = NULL;
+    int ret = 1;
+    int error;
+
+    /* Create the recording stream */
+    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
+        fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        goto finish;
+    }
+
+    for (;;) {
+        uint8_t buf[BUFSIZE];
+
+        /* Record some data ... */
+        if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) {
+            fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+            goto finish;
+        }
+
+        /* And write it to STDOUT */
+    	write(s, buf, sizeof(buf));
+    //    if (loop_write(STDOUT_FILENO, buf, sizeof(buf)) != sizeof(buf)) {
+    //        fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
+    //        goto finish;
+    //    }
+    }
+
+    ret = 0;
+
+finish:
+
+    if (s)
+        pa_simple_free(s);
+
+
+} 
+
+int main() 
+{ 
+	int s, connfd; 
+	struct sockaddr_in servaddr, cli; 
+
+	// socket create and varification 
+	s = socket(AF_INET, SOCK_STREAM, 0); 
+	if (s == -1) { 
+		printf("socket creation failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+	bzero(&servaddr, sizeof(servaddr)); 
+
+	// assign IP, PORT 
+	servaddr.sin_family = AF_INET; 
+	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+	servaddr.sin_port = htons(PORT); 
+
+	// connect the client socket to server socket 
+	if (connect(s, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
+		printf("connection with the server failed...\n"); 
+		exit(0); 
+	} 
+	else
+		printf("connected to the server..\n"); 
+
+	// function for chat 
+	func(s); 
+
+	// close the socket 
+	close(s); 
+} 
