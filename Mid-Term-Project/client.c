bf42df862862b7bc9cb9e12077c91e4e3ec13beb
sreekar <33954702+sreekarappana1998@users.noreply.github.com>
2020-06-06 10:05:39 +0530

    Update client.c

--- Mid-Term-Project/client.c
+++ Mid-Term-Project/client.c
@@ -1,86 +1,84 @@
-#include <netdb.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <string.h> 
-#include <sys/socket.h> 
-#define MAX 80 
-#define PORT 8080 
-#define SA struct sockaddr 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <stdlib.h>
+#include "pthread.h"
+#include <stddef.h>
+#include <pulse/simple.h>
+#include <pulse/error.h>
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
-
-#include <unistd.h>
-#include <errno.h>
-
-#include <pulse/simple.h>
-#include <pulse/error.h>
-#include <pulse/gccmacro.h>
-
 #define BUFSIZE 1024
 
-/* A simple routine calling UNIX write() in a loop */
-static ssize_t loop_write(int fd, const void*data, size_t size) {
-    ssize_t ret = 0;
-
-    while (size > 0) {
-        ssize_t r;
-
-        if ((r = write(fd, data, size)) < 0)
-            return r;
-
-        if (r == 0)
-            break;
-
-        ret += r;
-        data = (const uint8_t*) data + r;
-        size -= (size_t) r;
-    }
-
-    return ret;
-}
-
-
-void func(int s,int argc, char*argv[]) 
-{ 
-	//char buff[MAX]; 
-	int n; 
+int main(int argc, char *argv[]){
 	
-	/* The sample type to use */
-    static const pa_sample_spec ss = {
+    // The sample type to use
+	static const pa_sample_spec ss = {
         .format = PA_SAMPLE_S16LE,
         .rate = 44100,
         .channels = 2
     };
-    
+
     pa_simple *s = NULL;
-    int ret = 1;
-    int error;
+	int ret = 1;
+	int error;
 
-    /* Create the recording stream */
-    if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
-        fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
-        goto finish;
+    // Create the recording stream 
+    	
+
+	int client_socket;
+	struct sockaddr_in address_serv;
+
+	printf("\nCreating socket\n");
+	if((client_socket= socket(AF_INET, SOCK_STREAM, 0)) < 0){
+		perror("Socket error");
+		exit(1);
+	}
+	
+	memset(&address_serv, 0, sizeof(address_serv));
+	address_serv.sin_family = AF_INET;
+   	address_serv.sin_port = htons(9090);
+    
+    	if(inet_pton(AF_INET, argv[1], &address_serv.sin_addr)<=0){
+    		perror("pton error");
+    	} 
+
+	printf("\nWaiting for connection\n");
+    	if((connect(client_socket, (struct sockaddr *)&address_serv, sizeof(address_serv))) < 0){
+    		perror("failed connection...");
+    		exit(1);
+    	}
+    	printf("\nConnection established\n\n");
+    
+        if (!(s = pa_simple_new(NULL, argv[0], PA_STREAM_RECORD, NULL, "record", &ss, NULL, NULL, &error))) {
+        	fprintf(stderr, __FILE__": pa_simple_new() failed: %s\n", pa_strerror(error));
+        	goto finish;
+    	}
+
+    // Sending recorded data
+    	while(1){
+		uint8_t buff[BUFSIZE];
+		
+		/* Record some data ... */
+		if (pa_simple_read(s, buff, sizeof(buff), &error) < 0) {
+		    fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
+		    goto finish;
+		}
+                else{
+                  
+                  //pa_simple_write(s, buff, sizeof(buff), &error);
+                   send(client_socket, buff , sizeof(buff) , 0 ); 
+                    }
     }
-
-    for (;;) {
-        uint8_t buf[BUFSIZE];
-
-        /* Record some data ... */
-        if (pa_simple_read(s, buf, sizeof(buf), &error) < 0) {
-            fprintf(stderr, __FILE__": pa_simple_read() failed: %s\n", pa_strerror(error));
-            goto finish;
-        }
-
-        /* And write it to STDOUT */
-    	write(s, buf, sizeof(buf));
-    //    if (loop_write(STDOUT_FILENO, buf, sizeof(buf)) != sizeof(buf)) {
-    //        fprintf(stderr, __FILE__": write() failed: %s\n", strerror(errno));
-    //        goto finish;
-    //    }
-    }
-
+    
     ret = 0;
 
 finish:
@@ -88,34 +86,5 @@ finish:
     if (s)
         pa_simple_free(s);
 
-
-} 
-
-int main() 
-{ 
-	int s, connfd; 
-	struct sockaddr_in servaddr, cli; 
-
-	// socket create and varification 
-	s = socket(AF_INET, SOCK_STREAM, 0); 
-	if (s == -1) { 
-		printf("socket creation failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("Socket successfully created..\n"); 
-	bzero(&servaddr, sizeof(servaddr)); 
-
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
-	servaddr.sin_port = htons(PORT); 
-
-	// connect the client socket to server socket 
-	if (connect(s, (SA*)&servaddr, sizeof(servaddr)) != 0) { 
-		printf("connection with the server failed...\n"); 
-		exit(0); 
-	} 
-	else
-		printf("connected to the server..\n"); 
-} 
+    return ret;
+}
